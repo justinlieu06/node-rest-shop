9760df51fd4334487e7a1835d8dbe86a6684885c
JustinLieu06 <justinlieu06@gmail.com>
2020-01-21 22:46:35 -0800

    added products and orders routes

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'It works!'
-  });
-});
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+
+//if the url starts with /products, then use productRoutes which can be found by the path in require
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+
+// app.use((req, res, next) => {
+//   res.status(200).json({
+//     message: 'It works!'
+//   });
+// });
 
 module.exports = app;
