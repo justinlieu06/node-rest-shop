80c9232543088715257ae5266442b5b7c1034c28
JustinLieu06 <justinlieu06@gmail.com>
2020-01-22 23:57:19 -0800

    added comments

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const orderRoutes = require('./api/routes/orders');
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
-
+//normal template
 // app.use((req, res, next) => {
 //   res.status(200).json({
 //     message: 'It works!'
